28c5e85576eb49dc721e209524f36dbeb91548b4
Josh Elser <josh.elser@gmail.com>
2010-03-03 19:21:51 -0500

    We want to use freq mode detection

--- pitchdetection.cpp
+++ pitchdetection.cpp
@@ -4,13 +4,14 @@
 #include <aubio/aubio.h>
 #include <aubio/sndfileio.h>
 #include <aubio/pitchdetection.h>
+#include <aubio/pitchyinfft.h>
 #include <aubio/jackio.h>
 
 using namespace std;
 
 /* Values for pitchdetection object */
-aubio_pitchdetection_type detection_type = aubio_pitch_fcomb;
-aubio_pitchdetection_mode detection_mode = aubio_pitchm_midi;
+aubio_pitchdetection_type detection_type = aubio_pitch_yinfft;
+aubio_pitchdetection_mode detection_mode = aubio_pitchm_freq;
 uint_t buffer_size = 1024;
 uint_t overlap_size = 256;
 uint_t channels = 2;
@@ -19,12 +20,17 @@ uint_t samplerate = 44100;
 // Already declared
 //uint_t overlap_size = 256;
 
+aubio_onset_t *o;
+
 smpl_t pitch = 0.;
 
 unsigned int pos = 0;
 
+aubio_pitchdetection_t *pitchdetect;
 fvec_t *ibuf;
 fvec_t *obuf;
+fvec_t *onset;
+fvec_t *pitch_obuf;
 
 static void process_print (void) {
     cout << "Current pitch: " << pitch << endl;
@@ -42,6 +48,15 @@ static int aubio_process (smpl_t **input, smpl_t **output, int nframes)
             /* put synthnew in output */
             output[i][j] = fvec_read_sample(obuf, i, pos);
         }
+
+        if (pos == overlap_size-1)
+        {
+            /* Block loop ?? */
+            //aubio_onset_do(o, ibuf, onset);
+
+            //aubio_pitch_do (pitchdetect, ibuf, pitch_obuf);
+
+        }
     }
 
     return 1;
@@ -49,7 +64,7 @@ static int aubio_process (smpl_t **input, smpl_t **output, int nframes)
 
 int main (int argc, char **argv)
 {
-    aubio_pitchdetection_t *pitchdetect = new_aubio_pitchdetection (buffer_size, overlap_size, channels, samplerate, detection_type, detection_mode);
+    pitchdetect = new_aubio_pitchdetection (buffer_size, overlap_size, channels, samplerate, detection_type, detection_mode);
 
     aubio_pitchdetection_set_yinthresh(pitchdetect, 0.7);
 
